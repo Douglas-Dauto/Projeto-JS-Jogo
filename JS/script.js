4541cab537b6c1eb24e3986c2a170681bd698f8b
Douglas Bastos <dautodouglasdauto@gmail.com>
2023-06-10 15:10:25 -0300

    Refatorado o script.js e o style.scss

--- JS/script.js
+++ JS/script.js
@@ -5,21 +5,26 @@ function jogo() {
     const corpo = document.getElementsByTagName('body')[0];
     const lua = document.getElementById('lua');
     const menu = document.getElementById('menu');
-    let c = 0
+    let qtdAdiversarios = 0;
+    let move = true;
     
     menu.innerHTML = '<p>Clique para iniciar!</p>';
+
+    setInterval(() => {
+        if(move === false) { 
+            gerarAdversario(qtdAdiversarios)
+        }}, 5000);
     
     corpo.addEventListener('mousedown', () => {
         menu.innerHTML = '';
 
-        if (c == 0) {
-            lua.style.transform += 'translateX(-100vw)'
-            c++
+        if (move) {
+            lua.style.transform = 'translateX(-100vw)'
+            gerarAdversario(qtdAdiversarios);
+            move = false;
         }
     
-        for(i = 0;i < 150;i++) {
-            personagem.style.transform = 'rotate(-5deg) translateY(-300px)'
-        }
+        personagem.style.transform = 'rotate(-5deg) translateY(-300px)'
     });
     
     corpo.addEventListener('mouseup', () => {
@@ -28,7 +33,17 @@ function jogo() {
         }
     });
 
-    function gerarAdversario() {
+    function gerarAdversario(numAdversario) {
+        adversario.innerHTML = `<img id="adversario-img${numAdversario}" class="adversario__img" src="assets/img/geometry-dash-balls-png-download-geometry-dash-icons (2).png" alt="Adversário">`;
+        
+        let adversarioImg = document.getElementById(`adversario-img${numAdversario}`);
+        adversarioImg.style.top = `${Math.random() * 60 + 10}%`;
 
+        setTimeout(moverAdversario, 300);
+
+        function moverAdversario() {
+            adversarioImg.style.transform = 'translateX(-120vw) rotateY(180deg)';
+            qtdAdiversarios++;
+        }
     }
 }
